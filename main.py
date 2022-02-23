459ea12d6b7398b1cb8d117947a62578695994c8
NotYahir <Yahirhernandez304@gmail.com>
2022-02-22 18:06:11 -0600

    New TicTacToe Code

--- main.py
+++ main.py
@@ -4,14 +4,58 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+global board
+board = [
+    ["___", "|___|", "___"],
+    ["___", "|___|", "___"],
+    ["   ", "|   |", "   "]
 
+]
+
+
+def create_board2():
+    j=0
+    for i in range(3):
+        if j != 2:
+            print("    |    |    ")
+            print("____|____|____")
+        else:
+            print("    |    |    ")
+            print("    |    |    ")
+        j+=1
+
+
+def create_board():
+    for row in board:
+        for slot in row:
+            print(f"{slot}", end="")
+        print()
+
+
+def create_board3():
+    print(board)
+def create(x,y,test):
+   global board
+   board[x][y] = test
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
     print("Helo world")
+    create_board()
+    print()
+    '''
+    create_board2()
+    print()
+    '''
+
+
+    create(0,0,"_X_")
+    create_board()
+
+    print()
+
+    create(0,1,"|_O_|")
+    create_board()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
