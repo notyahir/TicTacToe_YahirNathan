37295d8b4bcdc80d1e289d21a8426662bf2406b5
NotYahir <Yahirhernandez304@gmail.com>
2022-02-24 12:19:24 -0600

    Organization and Refining
    Refined the create_board function to allow for comprehension and reduce complexity.
    
    Code was also organized so it is connected and I added more comments for clarity.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# This is a sample Python script.
-
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+"""
+Name: Nathan and Yahir
+Project: Tic-Tac-Toe
+"""
 
+import random
+
+# This global variable, board, is the foundation of our program. It is a list of lists of strings. Since lists
+# are mutable, this makes it easy to actually "alter" the board.
 global board
 board = [
     ["___", "|___|", "___"],
@@ -13,41 +19,57 @@ board = [
 ]
 
 
-def create_board2():
-    j=0
-    for i in range(3):
-        if j != 2:
-            print("    |    |    ")
-            print("____|____|____")
-        else:
-            print("    |    |    ")
-            print("    |    |    ")
-        j+=1
+# Function creating the board! Prints each list of strings and we use the join function to get rid
+# of the list layout
 
 
 def create_board():
-    for row in board:
-        for slot in row:
-            print(f"{slot}", end="")
-        print()
+    for i in range(len(board)):
+        print("".join(board[i]))
 
 
-def create_board3():
-    print(board)
+# Function used to determine which player is going to go first
+
+def player_turn():
+    player_first = random.randint(1,2)
+    if player_first == 1:
+        print("The player will go first")
+    else: # (If playerFirst == 2 is the only other option)
+        print("The computer will go first")
+    return player_first
+
+
+# Will make the game commence!
+def game_run():
+    game_running = True
+
+    # Creates the board using the create_board function
+    create_board()
+    print()
+
+    player_turn()
+
+    while game_running:
+        if player_turn() == 1:
+            pass
+
+
+# This function modifies the tic-tac-toe board by allowing us to add the user buttons
 def create(x,y,test):
+   #Calling the global board function since this is what we want to alter.
    global board
+
    board[x][y] = test
+
+
+# This clear variable will be used to "reset" the console and just get rid of any previous iteration.
+clear = "\n" * 100
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Helo world")
     create_board()
     print()
-    '''
-    create_board2()
-    print()
-    '''
-
 
     create(0,0,"_X_")
     create_board()
@@ -60,4 +82,6 @@ if __name__ == '__main__':
     print("balls")
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+"""
+END OF PROGRAM
+"""
